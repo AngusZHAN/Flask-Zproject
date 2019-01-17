1dc4538c2cfeff24d3749069f282392375bf34d8
AngusZHAN <573585136@qq.com>
2019-01-17 14:22:22 +0800

    完善了测试功能

--- app/__init__.py
+++ app/__init__.py
@@ -4,21 +4,29 @@ from flask_login import LoginManager
 from flask_pagedown import PageDown
 import config
 
-app = Flask(__name__)
-app.config.from_object(config)
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-login_manager = LoginManager(app)
+login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-pagedown = PageDown(app)
+pagedown = PageDown()
 
-from app import models
 
-from .auth import auth
-app.register_blueprint(auth, url_prefix='/auth')
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
 
-from .main import main
-app.register_blueprint(main)
+    db.init_app(app)
+    login_manager.init_app(app)
+    pagedown.init_app(app)
+
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    return app
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-from app import app
+from app import create_app
+import config
+
+app = create_app(config)
 
 app.run(debug = True)
